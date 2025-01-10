2364aa8fb2672eb1ef5e465c02ebf26c1b4d6024
Cesar Frick <frickmijares@google.com>
2025-01-10 15:38:16 -0500

    Update dap for Python

--- .config/nvim/lua/plugins/dap/dap-python.lua
+++ .config/nvim/lua/plugins/dap/dap-python.lua
@@ -1,19 +1,18 @@
 return {
-  "mfussenegger/nvim-dap-python",
-  lazy = true,
-  ft = "python",
-  dependencies = {
-    "mfussenegger/nvim-dap",
-  },
-  config = function()
-    local dap = require("dap")
-    require("dap-python").setup("python3")
+	"mfussenegger/nvim-dap-python",
+	ft = "python",
+	dependencies = {
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		local dap = require("dap")
+		require("dap-python").setup("python")
 
-    table.insert(dap.configurations.python, {
-      type = "python",
-      request = "launch",
-      name = "Launch file",
-      program = "${file}",
-    })
-  end,
+		table.insert(dap.configurations.python, {
+			type = "python",
+			request = "launch",
+			name = "Launch file",
+			program = "${file}",
+		})
+	end,
 }
--- .config/nvim/lua/plugins/dap/dap.lua
+++ .config/nvim/lua/plugins/dap/dap.lua
@@ -1,55 +1,60 @@
 return {
-	"mfussenegger/nvim-dap",
-	lazy = true,
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"theHamsta/nvim-dap-virtual-text",
-		"nvim-telescope/telescope-dap.nvim",
-		"nvim-neotest/nvim-nio",
-		-- "leoluz/nvim-dap-go",
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
-		local map_opts = { noremap = true, silent = true }
-		local map = vim.keymap.set
+  "mfussenegger/nvim-dap",
+  lazy = true,
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "theHamsta/nvim-dap-virtual-text",
+    "nvim-telescope/telescope-dap.nvim",
+    "nvim-neotest/nvim-nio",
+    -- "leoluz/nvim-dap-go",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+    -- local map_opts = { noremap = true, silent = true }
+    local map = vim.keymap.set
 
-		local map_table = function(initial_table, key, value)
-			local result = initial_table
-			result[key] = value
+    -- local map_table = function(initial_table, key, value)
+    --   local result = initial_table
+    --   result[key] = value
+    --
+    --   print(result)
+    --
+    --   return result
+    -- end
 
-			print(result)
+    require("telescope").load_extension("dap")
+    -- require("dap-go").setup()
 
-			return result
-		end
+    dapui.setup()
 
-		require("telescope").load_extension("dap")
-		-- require("dap-go").setup()
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
 
-		dapui.setup()
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
+    dap.listeners.after.event_terminated.dapui_config = function()
+      dapui.close()
+    end
 
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
+    dap.listeners.after.event_exited.dapui_config = function()
+      dapui.close()
+    end
 
-		dap.listeners.after.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-
-		dap.listeners.after.event_exited.dapui_config = function()
-			dapui.close()
-		end
-
-		map("n", "<leader>db", dap.toggle_breakpoint, map_table(map_opts, "desc", "Toggle breakpoint"))
-		map("n", "<leader>dc", dap.continue, map_table(map_opts, "desc", "Continue"))
-		map("n", "<leader>di", dap.step_into, map_table(map_opts, "desc", "Step into"))
-		map("n", "<leader>do", dap.step_over, { desc = "Step over" })
-		map("n", "<leader>dO", dap.step_out, { desc = "Step out" })
-		map("n", "<leader>dC", dap.close, { desc = "Close" })
-		map("n", "<leader>dt", dap.terminate, { desc = "Terminate session" })
-	end,
+    map(
+      "n",
+      "<leader>db",
+      "<cmd>DapToggleBreakpoint<CR>",
+      { noremap = true, silent = true, desc = "Toggle breakpoint" }
+    )
+    map("n", "<leader>dc", "<cmd>DapContinue<CR>", { noremap = true, silent = true, desc = "Continue" })
+    map("n", "<leader>di", "<cmd>DapStepInto<CR>", { noremap = true, silent = true, desc = "Step into" })
+    map("n", "<leader>do", "<cmd>DapStepOver<CR>", { noremap = true, silent = true, desc = "Step over" })
+    map("n", "<leader>dO", "<cmd>DapStepOut<CR>", { noremap = true, silent = true, desc = "Step out" })
+    map("n", "<leader>dC", "<cmd>DapClose<CR>", { noremap = true, silent = true, desc = "Close" })
+    map("n", "<leader>dt", "<cmd>DapTerminate<CR>", { noremap = true, silent = true, desc = "Terminate session" })
+  end,
 }
