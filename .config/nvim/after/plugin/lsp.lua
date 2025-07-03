062ae017824a2a5a05f71fa7b53c1923a0ae80b6
Cesar Frick <frickmijares@google.com>
2025-07-03 08:22:55 -0500

    Remove keymaps and set virtual lines toggle

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -7,26 +7,24 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 		local snacks = require("snacks")
 
-		map("gd", snacks.picker.lsp_definitions, "Goto Definition")
-		map("gr", snacks.picker.lsp_references, "Goto Reference")
-		map("<leader>D", snacks.picker.lsp_type_definitions, "Type Definition")
-		map("<leader>ds", snacks.picker.lsp_symbols, "Document Symbols")
-		map("<leader>ws", snacks.picker.lsp_workspace_symbols, "Workspace Symbols")
-		map("<leader>rn", vim.lsp.buf.rename, "Rename")
-		map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
-		map("<leader>q", vim.diagnostic.setloclist, "Quickfix")
-		map("<leader>lo", vim.diagnostic.open_float, "Open Float")
+		-- map("gd", snacks.picker.lsp_definitions, "Goto Definition")
+		-- map("gr", snacks.picker.lsp_references, "Goto Reference")
 		map("K", function()
 			vim.lsp.buf.hover({ border = "rounded" })
 		end, "Hover Documentation")
-		map("gD", vim.lsp.buf.declaration, "Goto Declaration")
 		map("gI", vim.lsp.buf.implementation, "Goto Implementation")
 		map("<leader>uv", function()
-			local virtual_lines_config = not vim.diagnostic.config().virtual_lines
-			vim.diagnostic.config({ virtual_lines = virtual_lines_config, virtual_text = not virtual_lines_config })
+			local virtual_lines_config = not vim.diagnostic.config().virtual_lines.current_line
+			vim.diagnostic.config({
+				virtual_lines = { current_line = virtual_lines_config },
+			})
 		end, "Toggle virtual lines")
 		-- map("gT", vim.lsp.buf.type_definition, "Goto Type Definition")
 		-- map("gR", vim.lsp.buf.references, "Goto References")
 		-- map("gl", vim.lsp.buf.format, "Format")
 	end,
+})
+
+vim.lsp.config("*", {
+	capabilities = require("blink.cmp").get_lsp_capabilities(),
 })
